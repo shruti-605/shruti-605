4aaef4e12650d610ad7e1c9f00acbcc22f8d737c
shruti-605 <114794288+shruti-605@users.noreply.github.com>
2022-12-20 23:32:19 +0530

    Add files via upload

---
+++ hii.py
@@ -0,0 +1,8 @@
+#take two string inputs from the user x and y
+#your task is to
+# add two string x and y and assign the resultant value to other variable z
+# print the values in x y z in the exact foramt which is visible in tnhe sampletest cases
+x=str(input("x:"))
+y=str(input("y:"))
+z=x+y
+print("x=code,y=Tantra,z=codeTantra")
